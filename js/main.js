a03cfce4c096f854bd53c064b0177b0e050902d1
Mike Jones <michaeljones401@gmail.com>
2020-08-07 16:32:13 +0100

    adding all content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  $('.essay_link').click(function(){
+    var ref = $(this).data('ref');
+    $('.ref').removeClass('active_link')
+    $('#'+ref).addClass('active_link');
+    $('html,body').animate({
+      scrollTop: $("#"+ref).offset().top - 30
+   });
+  });
+
+});
